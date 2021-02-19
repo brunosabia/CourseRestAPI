d51e80c0a661debf01fac01d6f1ba276760a6b95
Bruno <brunosabia1@gmail.com>
2021-02-19 15:40:31 -0300

    Adding Classes 27

---
+++ UdemySpringBoot/14 RestWithSpringBootUdemy Scaffold Versioning your API/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -0,0 +1,70 @@
+package br.com.erudio.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.erudio.data.vo.PersonVO;
+import br.com.erudio.data.vo.v2.PersonVOV2;
+import br.com.erudio.services.PersonService;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+		
+		@Autowired
+		private PersonService services;
+		
+		//caso não especifique o value pro mapping, ele vai cair direto neste método quando entrar no "/person"
+		@GetMapping //forma mais enxuta de importar o GET
+		public List<PersonVO> findAll(){
+			return services.findAll();	
+		}
+		
+		@GetMapping("/{id}")
+		public PersonVO findById(@PathVariable("id")Long id){
+			return services.findById(id);	
+		}
+		
+		@PostMapping 
+		public PersonVO create(@RequestBody PersonVO person){
+			return services.create(person);	
+		}
+		
+		@PostMapping("v2")
+		public PersonVOV2 createV2(@RequestBody PersonVOV2 person){
+			return services.createV2(person);	
+		}
+		
+		
+		@PutMapping  
+		public PersonVO update(@RequestBody PersonVO person){
+			return services.update(person);	
+		}
+		
+		
+		@DeleteMapping("/{id}") 
+		public ResponseEntity<?> delete(@PathVariable("id")Long id){
+			 services.delete(id);	
+			 return ResponseEntity.ok().build();  //Adicionamos um retorno para o delete tb
+		}
+		
+		
+}
+
+
+//http://localhost:8080/person/1
+
+
+//vamos REFATORAR o nome dessa classe, selecione o nome da classe "GreetingController" e aperte alt+shift+R para então renomear a classe e se optar, arquivo, métodos... etc.
+
+//Para organizar as importações  pressione Ctrl + Shift + O
---
+++ UdemySpringBoot/14 RestWithSpringBootUdemy Scaffold Versioning your API/src/main/java/br/com/erudio/services/PersonService.java
@@ -0,0 +1,71 @@
+package br.com.erudio.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.erudio.Exception.ResourceNotFoundException;
+import br.com.erudio.converter.DozerConverter;
+import br.com.erudio.data.model.Person;
+import br.com.erudio.data.vo.PersonVO;
+import br.com.erudio.data.vo.v2.PersonVOV2;
+import br.com.erudio.repository.PersonRepository;
+
+@Service 
+public class PersonService {
+	
+	@Autowired
+	PersonRepository repository;
+	
+	public PersonVO create(PersonVO person) {
+		var entity = DozerConverter.parseObject(person, Person.class);//recebe o param VO e transforma em entity
+		var vo = DozerConverter.parseObject(repository.save(entity), PersonVO.class); //salva a entity no repository e converte em VO novamente
+		return vo; //retorna o VO para o client
+	}
+	public PersonVOV2 createV2(PersonVOV2 person) {
+		var entity = DozerConverter.parseObject(person, Person.class);//recebe o param VO e transforma em entity
+		var vo = DozerConverter.parseObject(repository.save(entity), PersonVOV2.class); //salva a entity no repository e converte em VO novamente
+		return vo; //retorna o VO para o client
+	}
+	
+	
+	
+	public List<PersonVO> findAll() {			
+		return DozerConverter.parseListObjects(repository.findAll(), PersonVO.class); //ele vai ate o banco atraves do repos.findAll, traz uma lista de entidades
+		// e o Dozer converte essa lista em PersonVO e retorna para o client.
+	}
+	
+	public PersonVO findById(Long id) {
+		
+		var entity = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID")); //busca no banco a entity
+		return DozerConverter.parseObject(repository.save(entity), PersonVO.class); //converte a entity em VO
+	}
+	
+	public PersonVO update(PersonVO p) {
+		
+		var entity = repository.findById(p.getId()).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID")); //busca entity
+		
+		entity.setFirstName(p.getFirstName()); //ajusta a entity
+		entity.setLastName(p.getLastName());
+		entity.setAddress(p.getAddress());
+		entity.setGender(p.getGender());
+		
+		var vo =  DozerConverter.parseObject(repository.save(entity), PersonVO.class); //repos.save salva o obj e o dozer converte em VO novamente para retornar pro client
+		return vo;
+	}
+	
+		public void delete(Long id) {
+			Person entity = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+			repository.delete(entity);
+		}
+	
+
+
+	
+	
+
+
+	
+	
+}
