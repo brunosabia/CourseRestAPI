bfec2364608ee0c6c5dfebc3c0b3ea2b1d15ce48
Bruno <brunosabia1@gmail.com>
2021-02-26 15:41:47 -0300

    Adding Classes 33

--- UdemySpringBoot/20 RestWithSpringBootUdemy Content Negotiation via QueryParam/src/main/java/br/com/erudio/config/WebConfig.java
+++ UdemySpringBoot/20 RestWithSpringBootUdemy Content Negotiation via QueryParam/src/main/java/br/com/erudio/config/WebConfig.java
@@ -12,10 +12,23 @@ public class WebConfig implements WebMvcConfigurer{
 
 	@Override //significa que vai sobreescrever o método anterior.
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		
+		/* CONTEUDO DA CLASSE 59
 		configurer.favorParameter(false) 
 		.ignoreAcceptHeader(false) //ele não irá ignorar o que vier no Header da requisição
 		.defaultContentType(MediaType.APPLICATION_JSON) //significa que o conteudo padrão será JSON
 		.mediaType("json", MediaType.APPLICATION_JSON) //e irá suportar os seguintes media types.
 		.mediaType("xml", MediaType.APPLICATION_XML);
+		*/
+		
+		configurer.favorPathExtension(false) //nesse codigo adicionado basicamente ele diz que nao aceita mais via Extension.
+		.favorParameter(true) //tornamos verdadeira a necessidade de um parametro que irá receber o tipo de content
+		.parameterName("mediaType") //demos o nome para esse parametro
+		.ignoreAcceptHeader(true) //ele irá ignorar o que vier no Header da requisição
+		.useRegisteredExtensionsOnly(false) //caso nenhum media type seja add, ele ainda assim rodará a app seguindo o defaultContentType.
+		.defaultContentType(MediaType.APPLICATION_JSON) //significa que o conteudo padrão será JSON
+		.mediaType("json", MediaType.APPLICATION_JSON) //e irá suportar os seguintes media types, JSON e XML
+		.mediaType("xml", MediaType.APPLICATION_XML);
+		
 	}
 }
