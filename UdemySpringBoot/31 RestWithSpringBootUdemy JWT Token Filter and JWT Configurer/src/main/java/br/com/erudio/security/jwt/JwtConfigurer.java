b5774fd69c695c588e0adc7ac091ef7b26b12f24
Bruno <brunosabia1@gmail.com>
2021-03-18 16:51:01 -0300

    Addind Classes 46

---
+++ UdemySpringBoot/31 RestWithSpringBootUdemy JWT Token Filter and JWT Configurer/src/main/java/br/com/erudio/security/jwt/JwtConfigurer.java
@@ -0,0 +1,28 @@
+package br.com.erudio.security.jwt;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class JwtConfigurer extends SecurityConfigurerAdapter<DefaultSecurityFilterChain,HttpSecurity> {
+
+	//Dependency Injection
+	@Autowired
+	private JwtTokenProvider tokenProvider;
+
+	//Constructor
+	public JwtConfigurer(JwtTokenProvider tokenProvider) {
+		this.tokenProvider = tokenProvider;
+	}
+	
+	@Override
+	public void configure(HttpSecurity http) throws Exception{
+		//Instancia um novo filtro para todo o tráfego HTTP
+		JwtTokenFilter customFilter = new JwtTokenFilter(tokenProvider);
+		//Adiciona esse filtro às politicas de segurança HTTP
+		http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
+	}
+	
+}
