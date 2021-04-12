f5e917d793a2d9201bfa70f9a16248fdd20b5f48
Bruno <brunosabia1@gmail.com>
2021-04-12 15:47:04 -0300

    Adding Classes 62

--- UdemySpringBoot/40 RestWithSpringBootUdemy Simultaneous Uploads/src/main/java/br/com/erudio/controllers/FileController.java
+++ UdemySpringBoot/40 RestWithSpringBootUdemy Simultaneous Uploads/src/main/java/br/com/erudio/controllers/FileController.java
@@ -4,13 +4,24 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.google.common.net.HttpHeaders;
 
 import br.com.erudio.data.vo.v1.UploadFileResponseVO;
 import br.com.erudio.services.FileStorageService;
@@ -21,6 +32,9 @@ import io.swagger.annotations.Api;
 @RequestMapping("/api/file/v1")
 public class FileController {
 
+	
+	private static final Logger logger = LoggerFactory.getLogger(FileController.class); //injeção do logger apenas para retornar o log da file de download no tratamento de exceções
+	
 	@Autowired
 	private FileStorageService fileStorageService;
 	
@@ -46,5 +60,32 @@ public class FileController {
 					 .collect(Collectors.toList()); //Coleta as responses e monta uma lista com cada retorno.
 	}
 	
+	//método responsável pelo download da file. recebendo o fileName
+	@GetMapping("/downloadFile/{fileName:.+}") //a variável é colocada dentro das chaves e o :.+ é para mostrar que aceita qualquer tipo de extensão.
+	public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
+		//primeiramente vamos buscar a nossa file no disco.
+		
+		Resource resource = fileStorageService.loadFileAsResource(fileName); // envia o fileName e tras o path todo do file como um resource.
+		
+		String contentType = null; //iniciamos o contentType
+		
+		 try {
+			 
+			contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath()); //populamos o contentType
+		
+		 } catch (Exception e) {
+			logger.info("Could not determine file type.");//logger irá mandar o log desse erro.
+		}
+		 
+		 if(contentType == null) {
+			 contentType = "application/octet-stream"; //caso o contentType nao seja encontrado ou nao seja valido, este funcionará como um type genérico.
+		 }
+		
+		 //aqui nós estamos construindo nossa Response Entity
+		return ResponseEntity.ok()
+				.contentType(MediaType.parseMediaType(contentType))//setando nossa responseEntity com o media type que encontramos acima.
+				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=)\""+ resource.getFilename() + "\"") //setamos no header o anexo
+				.body(resource); //adicionamos o nosso resource(o file transformado em rosource) ao body da response
+	}
 	
 }
--- UdemySpringBoot/40 RestWithSpringBootUdemy Simultaneous Uploads/src/main/java/br/com/erudio/services/FileStorageService.java
+++ UdemySpringBoot/40 RestWithSpringBootUdemy Simultaneous Uploads/src/main/java/br/com/erudio/services/FileStorageService.java
@@ -6,11 +6,14 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import br.com.erudio.Exception.FileStorageException;
+import br.com.erudio.Exception.MyFileNotFoundException;
 import br.com.erudio.config.FileStorageConfig;
 
 @Service
@@ -40,7 +43,10 @@ public class FileStorageService {
 			if(fileName.contains("..")) {
 				throw new FileStorageException("Filename contains invalid path sequence " + fileName);
 			} 
+			
+			
 			//se passar por essa outra validação finalmente será armazenado em disco
+			//CASO NAO DESEJA ARMAZENAR local, as mudanças devem acontecer nessas 2 linhas abaixo.
 			
 			Path targetLocation = this.fileStorageLocation.resolve(fileName); //criando o Path targetLocation que irá receber o file
 			Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING); //copia o InputStream() que veio dentro do file  para a targetLocation Substituindo CASO já exista.
@@ -51,6 +57,24 @@ public class FileStorageService {
 		} catch (Exception e) {
 			throw new FileStorageException("Could not store the file " + fileName + ". Please try again.", e);
 		}
+	}
+	
+	public Resource loadFileAsResource(String fileName) {
+		try {
+			//CASO NAO DESEJA LER local, as mudanças devem acontecer nessas 2 linhas abaixo.
+			Path filePath = this.fileStorageLocation.resolve(fileName).normalize(); //o file path pega o nome do arquivo e o caminho até ele
+			Resource resource = new UrlResource(filePath.toUri()); //transforma o filePath em um um Resource
+			
+			if(resource.exists()) { //se esse resource existir
+				return resource; 
+			}
+			else { //se esse filePath não for válido:
+				throw new MyFileNotFoundException("File not found " + fileName);
+			}
+		} catch (Exception e) {
+			throw new MyFileNotFoundException("File not found " + fileName, e);
+		}
+		
 	}
 		
 }
